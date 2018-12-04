62cf5e686b57172398175c94864ee62090220b58
Aditya Naik <naik.aditya@gmail.com>
2018-12-04 11:48:54 +0100

    Adds navbar partial with sign_up and login links to index page, click on registration test passing

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -8,14 +8,10 @@ Given("I visit the landing page") do
     visit root_path
 end
 
-Given("I click {string}") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+Given("I click {string}") do |element|
+    click_on element
 end
 
 When("I fill in {string} field with {string}") do |string, string2|
-    pending # Write code here that turns the phrase above into concrete actions
-end
-
-When("I click {string}") do |string|
     pending # Write code here that turns the phrase above into concrete actions
 end
