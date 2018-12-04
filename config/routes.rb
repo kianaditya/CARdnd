06a1a8c81b8be6a94d517078033da679c122cebe
johannaanden <johannaanden@hotmail.com>
2018-12-04 19:58:12 +0100

    Uppdates factory bot, adds unit test för validations and factorybot, adds validation for automobile and refactors routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :automobile
+  resources :automobile, only: [:index]
   root controller: :car_card, action: :index
   devise_for :users
-  resources :welcome, only: :index
 end
--- spec/models/automobile_spec.rb
+++ spec/models/automobile_spec.rb
@@ -7,4 +7,17 @@ RSpec.describe Automobile, type: :model do
     it { is_expected.to have_db_column :year }
     it { is_expected.to have_db_column :price }
   end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :brand }
+    it { is_expected.to validate_presence_of :model } 
+    it { is_expected.to validate_presence_of :year } 
+    it { is_expected.to validate_presence_of :price } 
+  end
+
+  describe 'Factory' do
+    it 'should have valid Factory' do
+      expect(FactoryBot.create(:automobile)).to be_valid
+    end
+  end
 end
