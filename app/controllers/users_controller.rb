1e30889b72ec40862a16aefb40641408c736834e
Gregory Kallai <gerg.kallai@gmail.com>
2018-12-05 16:23:14 +0100

    Removes update action from users controller, refactors profile page, puts there the edit link from navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,4 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
     end
-    def update
-        @user = User.find(params[:id])
-        redirect_to user_path(current_user)
-    end
-
-    private
-    def user_params
-        params.require(current_user).permit(:firstname, :lastname, :drivingage, :age, :about, :phone, :address)
-    end
 end
