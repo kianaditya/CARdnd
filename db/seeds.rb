11d52cfeab89aa4c9440111b2e5335923c0edbdb
Gregory Kallai <gerg.kallai@gmail.com>
2018-12-08 16:43:02 +0100

    Changes seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,11 @@
 
 Automobile.destroy_all
 User.destroy_all
-User.create(id:2, first_name: 'Greg', last_name: 'Kallai',email: 'real@mail.com', password: 'password', address: 'awesome street', phone: '897765422',driving_age: 9,age: 30 )
-Automobile.create(brand: "Volvo", model: "V60", year: 1985, price: 540, user_id: 2)
-Automobile.create(brand: "Tolvo", model: "T01", year: 1990, price: 400, user_id: 2)
-Automobile.create(brand: "Polvo", model: "P60", year: 1981, price: 850, user_id: 2)
+Rental.destroy_all
+User.create(id:1, first_name: 'Owner', last_name: 'User',email: 'real@mail.com', password: 'password', address: 'awesome street', phone: '897765422',driving_age: 9,age: 30 )
+User.create(id:2, first_name: 'Rental', last_name: 'User',email: 'really@mail.com', password: 'password', address: 'awesome street', phone: '897765422',driving_age: 9,age: 30 )
+Automobile.create(id: 1, brand: "Volvo", model: "V60", year: 1985, price: 540, user_id: 1)
+Automobile.create(id:2, brand: "Tolvo", model: "T01", year: 1990, price: 400, user_id: 1)
+Automobile.create(id:3, brand: "Polvo", model: "P60", year: 1981, price: 850, user_id: 1)
+Rental.create(automobile_id:1, user_id:2)
+Rental.create(automobile_id:2, user_id:2)
